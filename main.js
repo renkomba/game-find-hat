bc9882676d8bf929b929201bb4e7264d4e0c4c31
renkomba <61875726+renkomba@users.noreply.github.com>
2022-05-18 23:15:26 -0400

    added 2 static functions to generate random grid & a prompt loop to make it playable in terminal

--- main.js
+++ main.js
@@ -17,6 +17,69 @@ class Field {
 
         console.log(gridStr);
     }
+
+    static getRandomNum(exceptionArr, max) {
+        let num = 0;
+
+        do {
+            num = Math.floor( Math.random() * max)
+        } while (exceptionArr.includes(num));
+
+        return num;
+    }
+
+    static generateField(height, width=height) {
+        [ height, width ] = [
+            height < 3 ? 3 : height,
+            width < 3 ? 3 : width
+        ];
+
+        let [ multipleOfFive, numHoles, numBoxes, box, iHoles, newGrid ] = [
+            5,
+            1,
+            width * height,
+            0,
+            [],
+            []
+        ];
+
+        const iHat = Field.getRandomNum( [0], numBoxes);
+
+        // determine num of holes
+        do {
+            multipleOfFive += 5;
+            numHoles++;
+        } while (multipleOfFive < numBoxes);
+
+        // set indexes of holes
+        for (let i = 0; i < numHoles; i++) {
+            let j = 0;
+
+            j = Field.getRandomNum( [0, iHat, ...iHoles], numBoxes );
+            iHoles[i] = j;
+        }
+
+        // fill new grid
+        for (let i = 0; i < height; i++) {
+            newGrid.push([]);
+
+            for (let j = 0; j < width; j++) {
+                if ( box === 0 ) {
+                    newGrid[i][j] = path;
+                } else if ( box === iHat ) {
+                    newGrid[i][j] = hat;
+                } else if ( iHoles.includes(box) ) {
+                    newGrid[i][j] = hole;
+                } else {
+                    newGrid[i][j] = field;
+                }
+
+                box++;
+            }
+        }
+
+        return newGrid;
+    }
 }
 
 let smallGame = new Field([
@@ -29,3 +92,15 @@ let smallGame = new Field([
 const name = prompt('Player name? ');
 console.log(`${name.toUpperCase()}'S BOARD`);
 smallGame.print();
+
+let changeBoard = '';
+do {
+    changeBoard = prompt('Change board (y/n) ? ');
+
+    if (changeBoard === 'y') {
+        smallGame.grid = Field.generateField( Math.floor( Math.random() * 10 ) );
+        smallGame.print();
+    } else {
+        smallGame.print();
+    }
+} while (changeBoard === 'y');

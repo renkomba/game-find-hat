213b3cce71777a06fa6f917a9195e8c75734e287
renkomba <61875726+renkomba@users.noreply.github.com>
2022-05-18 21:57:15 -0400

    created class with constructor & print method. Also added sample class instance for debugging

---
+++ main.js
@@ -0,0 +1,28 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+const hat = '^';
+const hole = 'O';
+const field = '░';
+const path = '*';
+
+class Field {
+    constructor(arr2d) {
+        this.grid = arr2d;
+    }
+
+    print() {
+        let gridStr = this.grid.map(
+            arr => arr.join('')
+        ).join('\n');
+        
+        console.log(gridStr);
+    }
+}
+
+let smallGame = new Field([
+    [path, field, hole],
+    [field, hole, field],
+    [field, hat, field]
+])
+
+smallGame.print();
